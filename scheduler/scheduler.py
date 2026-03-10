5ef1534f92c129a01c5bd59ffc491582021dba2c
v2-2v <dev@yoimiya.net>
2026-03-11 01:22:38 +0900

    add about redis

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,10 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import redis
 
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 scheduler = BlockingScheduler()
 
 def job():
     print("run job")
-    
+
 print("start schedule")
 scheduler.add_job(job, "cron", hour=0, minute=0)
 scheduler.start()
