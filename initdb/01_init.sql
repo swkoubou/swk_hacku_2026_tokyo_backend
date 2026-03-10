c92a31e736c5da0dbd85fac155f208589e093fa2
v2-2v <dev@yoimiya.net>
2026-03-11 00:46:59 +0900

    timezone設定

--- initdb/01_init.sql
+++ initdb/01_init.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (user_uuid, name) VALUES --for dev
-('0bc63500-b97a-45cd-9b93-ee7ca7ce50f2', 'user1'),
-('3c7a9a24-9e34-4f65-bc1e-9a6e6c7d7f12', 'readme_user');
+('0bc63500-b97a-45cd-9b93-ee7ca7ce50f2', 'user1'), --sampple 削除予定
+('3c7a9a24-9e34-4f65-bc1e-9a6e6c7d7f12', 'readme_user'); --削除予定
 
 CREATE TABLE IF NOT EXISTS events (
     task_id UUID NOT NULL PRIMARY KEY,
