0f25991b88017981fd774aaee5ab5c5db14ee0ed
v2-2v <dev@yoimiya.net>
2026-03-12 00:35:38 +0900

    スケジューラー完成

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,11 +1,52 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import redis
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from datetime import date
+import json
+
+connection = psycopg2.connect("host=postgres dbname=calendar user=app password=app")
+cursor = connection.cursor(cursor_factory=RealDictCursor)
 
 r = redis.Redis(host="redis", port=6379, decode_responses=True)
+
 scheduler = BlockingScheduler()
 
 def job():
     print("run job")
+    try: 
+        cursor.execute(
+            """
+            SELECT
+                task_id,
+                user_uuid,
+                TO_CHAR(start_date, 'YYYY-MM-DD') as start_date,
+                TO_CHAR(start_time, 'HH24:MI:SS') as start_time,
+                TO_CHAR(end_date, 'YYYY-MM-DD') as end_date,
+                event_name
+            FROM events
+            WHERE start_date = %s
+            """,
+            (date.today(),)
+        )
+        query_result = cursor.fetchall()
+    except Exception as e:
+        print(e)
+    sum=0
+    pipe = r.pipeline()
+    for event in query_result:
+        data={
+            "user_uuid": event["user_uuid"],
+            "task_uuid": event["task_id"],
+            "start_date": event["start_date"],
+            "start_time": event["start_time"],
+            "end_date": event["end_date"],
+            "event_name": event["event_name"]
+        }
+        pipe.set(f"event:{event['user_uuid']}:{event['task_id']}", json.dumps(data), ex=86400)
+        sum+=1
+    pipe.execute()
+    print("write",sum," ",date.today())
 
 print("start schedule")
 scheduler.add_job(job, "cron", hour=0, minute=0)
